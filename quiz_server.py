0e362adbbb42988e85a4aa3155d178d477ca684f
Adhiraj16a12 <64204448+Adhiraj16a12@users.noreply.github.com>
2023-01-14 17:10:40 +0530

    Update quiz_server.py

--- quiz_server.py
+++ quiz_server.py
@@ -11,6 +11,94 @@ server.bind((ip_address,port_number))
 server.listen()
 print("server has started")
 clients = []
+import socket
+from threading import Thread
+from tkinter import *
+
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = '127.0.0.1'
+port = 8000
+
+client.connect((ip_address, port))
+
+print("Connected with the server...")
+
+class GUI:
+    def __init__(self):
+        self.Window = Tk()
+        self.Window.withdraw()
+
+        self.login = Toplevel()
+        self.login.title("Login")
+
+        self.login.resizable(width=False, height=False)
+        self.login.configure(width=400, height=300)
+        
+        self.pls = Label(self.login,
+					text = "Please login to continue",
+					justify = CENTER,
+					font = "Helvetica 14 bold")
+        self.pls.place( relheight = 0.15,
+                        relx = 0.2,
+                        rely = 0.07)
+
+        self.labelName = Label(self.login,
+							text = "Name: ",
+							font = "Helvetica 12")
+        self.labelName.place(   relheight = 0.2,
+							    relx = 0.1,
+							    rely = 0.2)
+
+        self.entryName = Entry(self.login,
+							font = "Helvetica 14")
+        self.entryName.place(relwidth = 0.4,
+							relheight = 0.12,
+							relx = 0.35,
+							rely = 0.2)
+        self.entryName.focus()
+
+        self.go = Button(self.login,
+						text = "CONTINUE",
+						font = "Helvetica 14 bold",
+						command = lambda: self.goAhead(self.entryName.get()))
+        self.go.place(  relx = 0.4,
+					    rely = 0.55)
+        
+        self.Window.mainloop()
+
+    def goAhead(self, name):
+        self.login.destroy()
+        self.name = name
+        rcv = Thread(target=self.receive)
+        rcv.start()
+
+    def receive(self):
+        while True:
+            try:
+                message = client.recv(2048).decode('utf-8')
+                if message == 'NICKNAME':
+                    client.send(self.name.encode('utf-8'))
+                else:
+                    pass
+            except:
+                print("An error occured!")
+                client.close()
+                break
+
+g = GUI()
+
+def write():
+    while True:
+         message = '{}: {}'.format(nickname, input(''))
+         client.send(message.encode('utf-8'))
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+write_thread = Thread(target=write)
+write_thread.start()
 
 questions = [
     " What is the Italian word for PIE? \n a.Mozarella\n b.Pasty\n c.Patty\n d.Pizza",
